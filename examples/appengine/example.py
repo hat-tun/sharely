6acd273c09b85b802b5f245c52eb13f8abbca74f
Naoki Hatta <naohatta69@gmail.com>
2012-05-06 22:37:40 +0900

    Initial commit. Working Facebook API's example application.

---
+++ examples/appengine/example.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+#
+# Copyright 2010 Facebook
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""
+A barebones AppEngine application that uses Facebook for login.
+Make sure you add a copy of facebook.py (from python-sdk/src/) into this
+directory so it can be imported.
+"""
+
+FACEBOOK_APP_ID = "388713961173237"
+#FACEBOOK_APP_ID = "your app id"
+
+FACEBOOK_APP_SECRET = "b1e37f7dd9413361a6b224b66c6a4d51"
+#FACEBOOK_APP_SECRET = "your app secret"
+
+import facebook
+import os.path
+import wsgiref.handlers
+import logging
+import urllib2
+
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
+from google.appengine.api.urlfetch import fetch
+
+
+class User(db.Model):
+    id = db.StringProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    updated = db.DateTimeProperty(auto_now=True)
+    name = db.StringProperty(required=True)
+    profile_url = db.StringProperty(required=True)
+    access_token = db.StringProperty(required=True)
+
+
+class BaseHandler(webapp.RequestHandler):
+    """Provides access to the active Facebook user in self.current_user
+
+    The property is lazy-loaded on first access, using the cookie saved
+    by the Facebook JavaScript SDK to determine the user ID of the active
+    user. See http://developers.facebook.com/docs/authentication/ for
+    more information.
+    """
+    @property
+    def current_user(self):
+        if not hasattr(self, "_current_user"):
+            self._current_user = None
+            cookie = facebook.get_user_from_cookie(
+                self.request.cookies, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET)
+            if cookie:
+                # Store a local instance of the user data so we don't need
+                # a round-trip to Facebook on every request
+                user = User.get_by_key_name(cookie["uid"])
+                if not user:
+                    graph = facebook.GraphAPI(cookie["access_token"])
+                    profile = graph.get_object("me")
+                    user = User(key_name=str(profile["id"]),
+                                id=str(profile["id"]),
+                                name=profile["name"],
+                                profile_url=profile["link"],
+                                access_token=cookie["access_token"])
+                    user.put()
+                elif user.access_token != cookie["access_token"]:
+                    user.access_token = cookie["access_token"]
+                    user.put()
+                self._current_user = user
+        return self._current_user
+
+
+class HomeHandler(BaseHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), "example.html")
+        args = dict(current_user=self.current_user,
+                    facebook_app_id=FACEBOOK_APP_ID)
+        self.response.out.write(template.render(path, args))
+
+    def post(self):
+        url = self.request.get('url')
+        file = urllib2.urlopen(url)
+        graph = facebook.GraphAPI(self.current_user.access_token)
+        graph.put_photo(file, "Test Image")
+
+
+def main():
+    logging.getLogger().setLevel(logging.DEBUG)
+    util.run_wsgi_app(webapp.WSGIApplication([(r"/", HomeHandler)]))
+
+
+if __name__ == "__main__":
+    main()
